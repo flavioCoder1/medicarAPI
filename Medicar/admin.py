a30115e4cfd984c5db66815691518dbdfdf89a53
Flavio_Cordeiro <flavio_ribeirorc@hotmail.com>
2022-04-18 22:07:09 -0300

    feat: cria Procfile & config para deploy no Heroku

--- Medicar/admin.py
+++ Medicar/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Medico, Agenda
+from .models import Medico, Agenda, Consulta
 
 admin.site.register(Medico)
 admin.site.register(Agenda)
--- app_medicar/settings.py
+++ app_medicar/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -132,3 +134,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+django_heroku.settings(locals())
